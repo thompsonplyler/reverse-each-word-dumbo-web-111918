17b653e9d3cb6ecb4b041eb7f5209bc92c5f27f5
Thompson Plyler <nealok.g@gmail.com>
2018-11-06 00:10:05 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(sentence)
+sen_array = sentence.split(" ")
+reverse_array = []
+
+sen_array.each do |x|
+  reverse_array << x.reverse
+end
+
+reverse_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+sen_array = sentence.split(" ")
+
+sen_array.collect do |x|
+  x.reverse
+end.join(" ")
+
+end
+# for each item in sen_array, reverse the word
